98ac1c337ab228fdeba4349ff5e9bc5d0bc72fe6
Danny Lin <danny@kdrag0n.dev>
2026-05-07 10:26:03 -0700

    Stop leaving alternate screen in terminal repair

--- src/terminal.rs
+++ src/terminal.rs
@@ -25,7 +25,7 @@ pub fn repair_terminal() {
     let mut out = io::stdout();
     let _ = write!(
         out,
-        "\x1b[?1000l\x1b[?1002l\x1b[?1003l\x1b[?1006l\x1b[?1015l\x1b[?1004l\x1b[?2004l\x1b[?25h\x1b[?1049l\x1b[?47l\x1b[?1047l"
+        "\x1b[?1000l\x1b[?1002l\x1b[?1003l\x1b[?1006l\x1b[?1015l\x1b[?1004l\x1b[?2004l\x1b[?25h"
     );
     let _ = out.flush();
 }
